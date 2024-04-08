6e5159981f6d19bd2396c369e31130e151aa7fa5
Jefferson <jeffersongarzonm@gmail.com>
2024-04-07 23:23:55 -0500

    refactor stuff and support parenthesis tokens

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,56 @@
-use regex::Regex;
 use std::collections::LinkedList;
 
 #[derive(Debug)]
-enum Expression<'a> {
+enum Token {
     Number(f32),
-    Operator(Operation),
-    Compose(&'a Expression<'a>, Operation, &'a Expression<'a>),
-}
-
-#[derive(Debug)]
-enum Operation {
     Add,
     Substract,
     Multiply,
     Divide,
+    RParenthesis,
+    LParenthesis,
 }
 
-fn str_to_expression(s: &str) -> Expression<'static> {
-    let number_regex: Regex = Regex::new(r"\d+(\.\d+)?").unwrap(); // TODO: this could be a global
-                                                                   // thing
-
-    if number_regex.is_match(s) {
-        str_to_number(s)
-    } else {
-        str_to_operator(s)
-    }
-}
-
-fn str_to_number(s: &str) -> Expression<'static> {
+fn str_to_number_expr(s: &str) -> Token {
     match s.parse() {
-        Ok(num) => Expression::Number(num),
+        Ok(num) => Token::Number(num),
         Err(_) => panic!("unexpected number char {s}"),
     }
 }
 
-fn str_to_operator(s: &str) -> Expression<'static> {
-    let operation = match s {
-        "+" => Operation::Add,
-        "-" => Operation::Substract,
-        "*" => Operation::Multiply,
-        "/" => Operation::Divide,
+fn build_operation_expr(ch: char) -> Token {
+    match ch {
+        '+' => Token::Add,
+        '-' => Token::Substract,
+        '*' => Token::Multiply,
+        '/' => Token::Divide,
         other => panic!("unexpected operator char {other}"),
-    };
-
-    Expression::Operator(operation)
+    }
 }
 
-fn str_to_stack(s: &str) -> LinkedList<Expression> {
-    let mut stack: LinkedList<Expression> = LinkedList::new();
-    let mut expr_str = String::new();
+fn build_number_expr(initial_digit: char, chars: &Vec<char>, i: &mut usize) -> Token {
+    let mut number = String::new();
+    number.push(initial_digit);
+
+    while *i < chars.len() {
+        if chars[*i].is_digit(10) {
+            number.push(chars[*i]);
+            *i += 1;
+        } else {
+            break;
+        }
+    }
+
+    str_to_number_expr(&number)
+}
+
+fn str_to_list(s: &str) -> LinkedList<Token> {
+    let chars: Vec<char> = s.trim().chars().collect();
+    let mut tokens: LinkedList<Token> = LinkedList::new();
 
     let mut i = 0;
-    let chars: Vec<char> = s.trim().chars().collect();
-    let total_chars = s.trim().chars().count();
 
-    while i < total_chars {
+    while i < chars.len() {
         let ch = chars[i];
         i += 1;
 
@@ -62,29 +58,33 @@ fn str_to_stack(s: &str) -> LinkedList<Expression> {
             continue;
         }
 
-        expr_str.push(ch);
-
-        // if it is a number and also not the last char
-        if ch.is_digit(10) && i != total_chars {
-            let next = chars[i];
-
-            if next.is_digit(10) {
-                continue;
-            }
+        if ch == '(' {
+            tokens.push_back(Token::LParenthesis);
+            continue;
         }
 
-        let expr = str_to_expression(&expr_str);
+        if ch == ')' {
+            tokens.push_back(Token::RParenthesis);
+            continue;
+        }
 
-        stack.push_front(expr);
-        expr_str.clear();
+        if ch.is_digit(10) {
+            let number = build_number_expr(ch, &chars, &mut i);
+            tokens.push_back(number);
+            continue;
+        }
+
+        let operation = build_operation_expr(ch);
+
+        tokens.push_back(operation);
     }
 
-    stack
+    tokens
 }
 
 fn main() {
-    let input = "2 + 3 * 4 / 2"; // 8
-    let stack = str_to_stack(input);
+    let input = "(22 + 3) * 44 / 2"; // 550
+    let tokens = str_to_list(input);
 
-    println!("{:?}", stack);
+    println!("{:?}", tokens);
 }
